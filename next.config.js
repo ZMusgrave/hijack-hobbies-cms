66d0d39942ed7250b4a7a2e3e36624b1338d9d3f
Zach Musgrave <musgrave.zm99@gmail.com>
2024-04-19 10:49:20 -0700

    remove i18 packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 const nextComposePlugins = require('next-compose-plugins');
 
 const headers = require('./config/headers');
 const plugins = require('./config/plugins');
-const { i18n } = require('./next-i18next.config.js');
 
 /**
  * https://github.com/cyrilwanner/next-compose-plugins/issues/59
@@ -15,30 +14,18 @@ const { withPlugins } = nextComposePlugins.extend(() => ({}));
  * documentation: https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
 module.exports = withPlugins(plugins, {
-  i18n,
-  /**
+  /**3
    * add the environment variables you would like exposed to the client here
    * documentation: https://nextjs.org/docs/api-reference/next.config.js/environment-variables
    */
   env: {
     ENVIRONMENT_NAME: process.env.ENVIRONMENT_NAME,
   },
-
-  /**
-   * The experimental option allows you to enable future/experimental options
-   * like React 18 concurrent features.
-   */
   experimental: {
     // urlImports: true,
     // concurrentFeatures: true,
     // serverComponents: true,
   },
-
-  /**
-   * SWC minification opt-in
-   * Please note that while not in experimental, the swcMinification may cause issues in your build.
-   * example: https://github.com/vercel/next.js/issues/30429 (Yup email validation causes an exception)
-   */
   // swcMinify: true,
 
   poweredByHeader: false,
@@ -57,7 +44,7 @@ module.exports = withPlugins(plugins, {
    * Settings are the defaults
    */
   images: {
-    domains: ['images.ctfassets.net', 'images.eu.ctfassets.net'],
+    domains: ['images.ctfassets.net'],
   },
 
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
